bbe75c0ef306de9cf0be8bdd14859ac05413436c
raul3963 <raulsc396@gmail.com>
2022-11-15 22:08:29 -0300

    a
    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Jogos from './telas/jogos';
 
 function App() {
   return (
-    <Router>
       <div className="App">
         <header className="App-header">
           <h3>BINGO DO MEDO</h3>
         </header>
-        <Routes className="routes">
-          <Route path='/' element={<Jogos />}></Route>
-          <Route path='/jogo' element={<Jogos />}></Route>
-          <Route path='/curta'></Route>
-        </Routes>
+        <Jogos />
         <footer>por raul396</footer>
       </div>
-    </Router>
   );
 }
 
